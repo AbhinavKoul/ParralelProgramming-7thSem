0dda9d88631c7b35cb042cc100da156c8e2b97cd
AbhinavKoul <47354541+AbhinavKoul@users.noreply.github.com>
2021-11-18 00:52:16 +0530

    MPI

---
+++ MPI/6 searchInt.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+
+long F(long a)
+{
+  return a*a;
+}
+
+void search(long limit, long num, int id)
+{
+  long t = (long)(limit*id)/4;
+  long m = (limit*(id+1))/4;
+  
+  for(long i=t; i<=m; i++)
+  {
+    if(F(i)==num)
+    {
+      printf("Process %d found!",id);
+      break;
+    }
+  }
+
+}
+
+int main(){
+  int rank,num_procs;
+  MPI_Init(NULL,NULL);
+  MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+  search(100,49,rank);
+  MPI_Finalize();
+  return 0;
+}
