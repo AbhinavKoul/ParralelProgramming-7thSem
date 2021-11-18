a24a7dafc97235a9e912d905140aeeb0a2742d4a
AbhinavKoul <47354541+AbhinavKoul@users.noreply.github.com>
2021-11-18 14:58:03 +0530

    update

--- MPI/1 Communication bw 2 process.c
+++ MPI/1 Communication bw 2 process.c
@@ -1,35 +1,37 @@
 #include<stdio.h>
 #include<mpi.h>
 #include<string.h>
-#define BUFFER_SIZE 32
 
-int main(int argc, char* argv[])
+#define BUFFER 32
+
+int main()
 {
-	int rank,numprocs;
-	int tag=0,root=3,temp=1;
-	char msg[BUFFER_SIZE];
+	char msg[BUFFER];
 
-	MPI_Init(&argc,&argv);
-	MPI_Status status;
+	int rank,numprocs;
+
+	MPI_Init(NULL,NULL);
 	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 	MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
-	
-	if(rank==3)
+
+	MPI_Status status;
+
+	int tag =0,root=3;
+	if(rank == 3)
 	{
-		strcpy(msg,"Hello");
-		
-		for(temp=0;temp<numprocs;temp++)
+		strcpy(msg,"hello");
+
+		for(int temp = 0;temp<numprocs;temp++)
 		{
 			if(temp!=3)
-			{
-				MPI_Send(msg,BUFFER_SIZE,MPI_CHAR,temp,tag,MPI_COMM_WORLD);
-			}
+				MPI_Send(msg,BUFFER,MPI_CHAR,temp,tag,MPI_COMM_WORLD);
 		}
 	}
 	else
 	{
-		MPI_Recv(msg,BUFFER_SIZE,MPI_CHAR,root,tag,MPI_COMM_WORLD,&status);
-		printf("\n%s in process with rank %d from process with rank %d\n",msg,rank,root);
+		MPI_Recv(msg,BUFFER,MPI_CHAR,root,tag,MPI_COMM_WORLD,&status);
+		printf("\n %s in process with rank %d from the process %d \n",msg,rank,root);
 	}
+
 	MPI_Finalize();
 }
