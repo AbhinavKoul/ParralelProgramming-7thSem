f4d670920d5cccb66a0729fd1aac88b843616be4
AbhinavKoul <47354541+AbhinavKoul@users.noreply.github.com>
2021-11-18 15:36:21 +0530

    pi

--- MPI/3 Pi.c
+++ MPI/3 Pi.c
@@ -1,39 +1,32 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <mpi.h>
-#include <math.h>
-
-#define SEED 12345678
+#include<stdlib.h>
+#include<mpi.h>
+#include<stdio.h>
+#include<math.h>
 #define N 100000
 
 int main()
 {
-  int rank, size;
-  double x,y,z,val=0,count=0;
-
+  int rank,size;
   MPI_Init(NULL,NULL);
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+  MPI_Comm_size(MPI_COMM_WORLD,&size);
 
-  srand(SEED+rank);
-
-  for(int i=0;i<N;i++)
+  double count = 0,val = 0;
+  for(int i = 0;i<N;i++)
   {
-    x=(double)rand()/RAND_MAX;
-    y=(double)rand()/RAND_MAX;
+    double x = (double)rand()/RAND_MAX;
+    double y = (double)rand()/RAND_MAX;
 
-    z=x*x+y*y;
-    
+    double z = (x*x) + (y*y);
+
     if(z<=1)
-      count++;
-
+      count++; 
   }
 
-  MPI_Reduce(&count, &val,1,MPI_DOUBLE,MPI_SUM,0,MPI_COMM_WORLD);
+  MPI_Reduce(&count,&val,1,MPI_DOUBLE,MPI_SUM,0,MPI_COMM_WORLD);
 
-  if(rank==0)
-    printf("PI:%lf\t",val/N);
+  if(rank == 0)
+    printf("PI: %lf \t",val/N);
 
   MPI_Finalize();
-  
-  return 0;
 }
