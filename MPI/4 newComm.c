2327c85801f0a8d424d2e425d1daf1943fdd19b4
AbhinavKoul <47354541+AbhinavKoul@users.noreply.github.com>
2021-11-18 15:49:49 +0530

    sh

--- MPI/4 newComm.c
+++ MPI/4 newComm.c
@@ -1,36 +1,31 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <mpi.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<mpi.h>
 
 int main()
 {
-  int rank, size;
-  MPI_Comm even_comm,odd_comm;
-  MPI_Group even_group,odd_group,world_group;
-
+  int rank,size;
   MPI_Init(NULL,NULL);
   MPI_Comm_rank(MPI_COMM_WORLD,&rank);
   MPI_Comm_size(MPI_COMM_WORLD,&size);
-  MPI_Comm_group(MPI_COMM_WORLD, &world_group);
+  
+  MPI_Group world_grp,even_grp,odd_grp;
+  MPI_Comm even_comm,odd_comm;
+  MPI_Comm_group(MPI_COMM_WORLD,&world_grp);
 
-  int even_p = size/2;
-  int even_rank[2] = {0,2};
+  int even_p,odd_p;
+  even_p = odd_p = size/2;
+  int even_rank[2] = {0,2},odd_rank[2] = {1,3};
 
-  MPI_Group_incl(world_group,even_p,even_rank,&even_group);
-  MPI_Comm_create(MPI_COMM_WORLD,even_group,&even_comm);
+  MPI_Group_incl(world_grp,even_p,even_rank,&even_grp);
+  MPI_Comm_create(MPI_COMM_WORLD,even_grp,&even_comm);
+  MPI_Group_incl(world_grp,odd_p,odd_rank,&odd_grp);
+  MPI_Comm_create(MPI_COMM_WORLD,odd_grp,&odd_comm);
 
-  int odd_p = size/2;
-  int odd_rank[2] = {1,3};
-
-  MPI_Group_incl(world_group,odd_p,odd_rank,&odd_group);
-  MPI_Comm_create(MPI_COMM_WORLD,odd_group,&odd_comm);
-
-  if(rank==0){
-    printf("Even sum=%d\n",4);
-  }
-  else if(rank==1){
-    printf("Odd sum=%d\n",4);
-  }
+  if(rank == 0)
+    printf("Even Sum = %d\n",4);
+  else if(rank == 1)
+    printf("Odd Sum = %d\n",4);
 
   MPI_Finalize();
   return 0;
