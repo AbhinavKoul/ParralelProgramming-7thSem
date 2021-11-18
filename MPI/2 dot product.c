101bf32ac3d805bce39162f5332e0d7de5e0ca15
AbhinavKoul <47354541+AbhinavKoul@users.noreply.github.com>
2021-11-18 15:21:31 +0530

    dot up

--- MPI/2 dot product.c
+++ MPI/2 dot product.c
@@ -1,36 +1,32 @@
-#include<mpi.h>
 #include<stdio.h>
+#include<mpi.h>
 #include<stdlib.h>
 
-int main(int argc , char* argv[])
+int main()
 {
-    int rank , size, i ,j;
-    double a[100] , b[100];
-    double mysum , allsum;
+    double a[100],b[100];
 
-    MPI_Init(&argc, &argv);
+    int rank,size;
+    MPI_Init(NULL,NULL);
     MPI_Comm_rank(MPI_COMM_WORLD,&rank);
     MPI_Comm_size(MPI_COMM_WORLD,&size);
 
-    if( rank == 0 )
+    if(rank == 0)
         printf("\nStarting omp_dotprod with %d tasks",size);
-
-    for(i = 0; i < 100; i++ )
-	{
-        a[i] = 1.0;
-        b[i] = a[i];
-    }
     
-    mysum = 0.0;
-    
-	for(j = 0; j < 100; j++ )
-        mysum += a[j] * b[j];
+    for(int i = 0;i<100;i++)
+        b[i] = a[i] = 1.0;
 
-    printf("\nTask %d partial sum is %f",rank,mysum);
+    double mysum = 0.0,allsum;
+
+    for(int i = 0;i<100;i++)
+        mysum += a[i]*b[i];
+    
+    printf("\n Task %d partial sum is %f\n",rank,mysum);
     MPI_Reduce(&mysum,&allsum,1,MPI_DOUBLE,MPI_SUM,0,MPI_COMM_WORLD);
 
     if(rank == 0)
-        printf("\nCompleted process : global sum is %f\n",allsum);
-    
+        printf("\n Completed process : global sum is %f\n",allsum);
+
     MPI_Finalize();
 }
